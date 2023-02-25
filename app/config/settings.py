2455205ab655e599ad19eba59222dffa09e452f0
mikhail349 <mikhail349349@gmail.com>
2023-02-25 20:59:52 +0300

    initial

---
+++ app/config/settings.py
@@ -0,0 +1,39 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+from split_settings.tools import include
+
+load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+
+ALLOWED_HOSTS = ['127.0.0.1']
+
+ROOT_URLCONF = 'config.urls'
+
+WSGI_APPLICATION = 'config.wsgi.application'
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+STATIC_URL = 'static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+include(
+    'components/apps.py',
+    'components/middleware.py',
+    'components/templates.py',
+    'components/auth_password_validators.py',
+    'components/database.py',
+)
