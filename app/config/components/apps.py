47502981650e1c7fb5259f174d26e1d7969f369b
mikhail349 <mikhail349349@gmail.com>
2023-02-25 22:34:58 +0300

    flake8

--- app/config/components/apps.py
+++ app/config/components/apps.py
@@ -4,7 +4,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    
+    'django.contrib.staticfiles',    
     'rest_framework_jwt',
 ]
