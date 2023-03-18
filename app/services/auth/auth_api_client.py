2e20414627d440e74ec24cc0ce3543bc98cf084c
Anna <sudnitsina@gmail.com>
2023-03-18 13:10:57 +0300

    Add typehints into auth_api_client.py

--- app/services/auth/auth_api_client.py
+++ app/services/auth/auth_api_client.py
@@ -1,20 +1,26 @@
 import uuid
 from http import HTTPStatus
+from typing import Callable
 
 import requests
 from django.conf import settings
+from requests.exceptions import ConnectionError
 
 
-def with_token(func):
+def with_token(
+    func: Callable[["AuthClient", ...], requests.Response]
+) -> Callable[["AuthClient", ...], requests.Response]:
     """Декоратор для вызова метода и сипользованием токена.
     Если запрос с сохраненным токеном возвращает статус UNAUTHORIZED,
     запрашивается новый токен."""
-    def wrapper(self, *args, **kwargs):
+
+    def wrapper(self: AuthClient, *args, **kwargs) -> requests.Response:
         res = func(self, *args, **kwargs)
         if res.status_code == HTTPStatus.UNAUTHORIZED:
             self.token = self.get_token()
             res = func(self, *args, **kwargs)
         return res
+
     return wrapper
 
 
@@ -26,12 +32,15 @@ class AuthClient:
         self.login = settings.AUTH_USERNAME
         self.password = settings.AUTH_PASSWORD
         self.login_endpoint = self.url + settings.AUTH_LOGIN_ENDPOINT
-        self.user_roles_endpoint = (
-            self.url + settings.AUTH_USER_ROLE_ENDPOINT
-        )
+        self.user_roles_endpoint = self.url + settings.AUTH_USER_ROLE_ENDPOINT
         self.token = None
 
-    def get_token(self):
+    def get_token(self) -> str | None:
+        """Получить токен.
+
+        Returns: токен
+
+        """
         data = {
             "password": self.password,
             "username": self.login,
